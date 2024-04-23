cbed250e7918b593d7b2d26d3cca4c6b63757074
Himu Chowdhury <himu.chowdhury01@gmail.com>
2024-04-23 20:40:31 +0600

    Password Generator Project Added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1 className='text-center text-4xl text-white p-5'>Password Generator</h1>
+    </>
+  )
+}
+
+export default App
